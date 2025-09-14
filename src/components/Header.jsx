b76e4f050ea2921cbc4add4f9f8bd39ace8a7bb7
Sandun Pushpika <sandun.pushpika123@gmail.com>
2025-09-14 21:31:56 +0530

    Added logged user name in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,17 @@
 import { Menu } from "lucide-react";
+import { useEffect, useState } from "react";
+import { decodedValues } from "../utils/TokenService";
 
 const Header = ({ toggleSidebar, user }) => {
+  const [name, setName] = useState("Super Admin");
+
+  useEffect(() => {
+    const decodedToken = decodedValues();
+    if (decodedToken && decodedToken.firstName && decodedToken.lastName) {
+      setName(`${decodedToken.firstName} ${decodedToken.lastName}`);
+    }
+  });
+
   return (
     <div className="h-16 bg-white border-b flex items-center px-6 shadow-sm justify-between z-10">
 
@@ -16,7 +27,7 @@ const Header = ({ toggleSidebar, user }) => {
       {/* Right: User info */}
       <div className="flex items-center space-x-4">
         <div className="flex items-center space-x-2">
-          <span className="text-sm font-medium text-gray-700">{user?.name || "User"}</span>
+          <span className="text-sm font-medium text-gray-700">{name || "Super Admin"}</span>
           <img 
             src={`https://ui-avatars.com/api/?name=${encodeURIComponent(user?.name || "U")}&background=0D8ABC&color=fff`}
             alt="Avatar"
